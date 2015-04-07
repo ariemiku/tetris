cafede5c590b266633ff76f1aa65843c9aa0c502
ariemiku <arie@alfasystem.co.jp>
2015-04-07 11:43:32 +0900

    ゲームのシーンの切り替え

---
+++ Tetris/Assets/Script/Game.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// 押された瞬間の処理
+		// spaceキーでシーンを切り替える
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Application.LoadLevel("Title");
+		}
+	
+	}
+}
